abc2717315b7bd7499c5a276b0791f6b601413a6
wit.swps <wit.swps@protonmail.com>
2024-08-23 20:53:30 +0700

    Add SpeedInsights & Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,36 @@
 // This is the root layout component for your Next.js app.
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
 import { LayoutProps } from "@/.next/types/app/page";
-import { DM_Sans } from 'next/font/google'
-import { Space_Mono } from 'next/font/google'
-import { cn } from '@/lib/utils'
-import './globals.css'
+import { DM_Sans } from "next/font/google";
+import { Space_Mono } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const fontHeading = DM_Sans({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-heading',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-heading",
+});
 
 const fontBody = Space_Mono({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-body',
-  weight: '400'
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
+  weight: "400",
+});
 
 export default function Layout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body 
-        className={cn(
-          'antialiased',
-          fontHeading.variable,
-          fontBody.variable
-        )}
+      <body
+        className={cn("antialiased", fontHeading.variable, fontBody.variable)}
       >
+        <SpeedInsights />
+        <Analytics />
         {children}
       </body>
     </html>
-  )
+  );
 }
